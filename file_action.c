c7c5de6ae144e07dba00d91117bf0d3c605a48e2
hamzas04 <samraoui.hamza@gmail.com>
2023-05-26 19:53:05 -0700

    0x16. C - Simple Shell

--- file_action.c
+++ file_action.c
@@ -13,7 +13,7 @@ int proc_file_commands(char *file_path, int *exe_ret);
 
 int cant_open(char *file_path)
 {
-	char *error, *hist_str;
+	char *hist_str,*erreur;
 	int len;
 
 	hist_str = _itoa(hist);
@@ -21,24 +21,21 @@ int cant_open(char *file_path)
 		return (127);
 
 	len = _strlen(name) + _strlen(hist_str) + _strlen(file_path) + 16;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
+	erreur = malloc(sizeof(char) * (len + 1));
+	if (!erreur)
 	{
 		free(hist_str);
 		return (127);
 	}
 
-	_strcpy(error, name);
-	_strcat(error, ": ");
-	_strcat(error, hist_str);
-	_strcat(error, ": Can't open ");
-	_strcat(error, file_path);
-	_strcat(error, "\n");
-
+	sprintf(erreur, "%s: %s: Can't open %s\n", name, hist_str, file_path);
 	free(hist_str);
-	write(STDERR_FILENO, error, len);
-	free(error);
+
+	write(STDERR_FILENO, erreur, len);
+	free(erreur);
+
 	return (127);
+
 }
 
 /**
@@ -53,72 +50,81 @@ int cant_open(char *file_path)
  */
 int proc_file_commands(char *file_path, int *exe_ret)
 {
-	ssize_t file, b_read, i;
 	unsigned int line_size = 0;
-	unsigned int old_size = 120;
-	char *line, **args, **front;
-	char buffer[120];
-	int ret;
-
+	char *line = NULL;
+	FILE *fp;
+	int ret, i;
+	ssize_t b_read;
+	char **args; 
+	
 	hist = 0;
-	file = open(file_path, O_RDONLY);
-	if (file == -1)
+	fp = fopen(file_path, "r");
+	if (!fp)
 	{
 		*exe_ret = cant_open(file_path);
 		return (*exe_ret);
 	}
-	line = malloc(sizeof(char) * old_size);
-	if (!line)
-		return (-1);
+
 	do {
-		b_read = read(file, buffer, 119);
-		if (b_read == 0 && line_size == 0)
+		char *temp = NULL;
+		size_t temp_size = 0;
+
+		b_read = getline(&temp, &temp_size, fp);
+		if (b_read == -1 && line_size == 0)
+		{
+			free(temp);
 			return (*exe_ret);
-		buffer[b_read] = '\0';
+		}
+
+		line = _realloc(line, line_size, line_size + b_read + 1);
+		_strcpy(line + line_size, temp);
 		line_size += b_read;
-		line = _realloc(line, old_size, line_size);
-		_strcat(line, buffer);
-		old_size = line_size;
-	} while (b_read);
-	for (i = 0; line[i] == '\n'; i++)
-		line[i] = ' ';
-	for (; i < line_size; i++)
+		free(temp);
+
+	} while (b_read != -1);
+
+	fclose(fp);
+
+	for (i = 0; i < (int)line_size; i++)
 	{
 		if (line[i] == '\n')
 		{
 			line[i] = ';';
-			for (i += 1; i < line_size && line[i] == '\n'; i++)
-				line[i] = ' ';
+			while (line[i + 1] == '\n')
+				line[i++] = ' ';
 		}
 	}
+
 	variable_replacement(&line, exe_ret);
 	handle_line(&line, line_size);
+
+
 	args = _strtok(line, " ");
 	free(line);
+
 	if (!args)
 		return (0);
+
 	if (check_args(args) != 0)
 	{
 		*exe_ret = 2;
 		free_args(args, args);
 		return (*exe_ret);
 	}
-	front = args;
 
 	for (i = 0; args[i]; i++)
 	{
-		if (_strncmp(args[i], ";", 1) == 0)
+		if (_strcmp(args[i], ";") == 0)
 		{
-			free(args[i]);
 			args[i] = NULL;
-			ret = call_args(args, front, exe_ret);
-			args = &args[++i];
-			i = 0;
+			ret = call_args(args, args, exe_ret);
+			args = &args[i + 1];
+			i = -1;
 		}
 	}
 
-	ret = call_args(args, front, exe_ret);
+	ret = call_args(args, args, exe_ret);
 
-	free(front);
+	free_args(args, args);
 	return (ret);
 }
